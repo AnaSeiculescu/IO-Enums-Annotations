38a1bea08748392faf1e0fea5e6d3198e36d1339
Ana Seiculescu <anaseiculescu@gmail.com>
2025-01-18 17:41:24 +0200

    creating CSV file and reading the file inside the CSVParsing class

---
+++ src/main/java/org/example/CSVParsing.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CSVParsing {
+	List<String> standings;
+
+	Path path = Paths.get("src/main/resources/athletesResults.txt");
+
+	private void readFile() {
+		Path path = Paths.get("src/test/resources/fileTest.txt");
+
+		try {
+			List<String> standings = Files.readAllLines(path);
+			System.out.println(standings);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+
+}
